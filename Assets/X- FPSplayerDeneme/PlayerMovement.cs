b603ea6e28f4cf0a743a58751396f3ea802734e5
Abdullah Akyol <abdullah35akyol@gmail.com>
2023-10-20 17:13:21 +0300

    Third Part for Upload to Github

---
+++ Assets/X- FPSplayerDeneme/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public GameObject SlotPaneli;
+    public GameObject WinScreen;
+
+    public bool envanterAcikmi;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BitisNoktasi"))
+        {
+            WinScreen.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (envanterAcikmi == false)
+            {
+                SlotPaneli.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.Confined;
+
+                envanterAcikmi = true;
+            }
+            else if (envanterAcikmi == true)
+            {
+                SlotPaneli.SetActive(false);
+
+                Cursor.lockState = CursorLockMode.Locked;
+                envanterAcikmi = false;
+            }
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward *z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
